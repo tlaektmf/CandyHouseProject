ebdbda1917c3345f5d59d542a1a25b27f8e23c34
yoeri1123 <yoeri1123@naver.com>
2018-09-10 20:52:25 +0900

    font 리소스 추가

---
+++ 18_08_13/Assets/Scripts/SceneChangeDirector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChangeDirector : MonoBehaviour {
+    public void SceneChangeToSecondScene()
+    {
+        SceneManager.LoadScene("SecondScene");
+    }
+    public void SceneChangeToSettingScene()
+    {
+        SceneManager.LoadScene("settingScene");
+    }
+}
